e1afce34221f0d9dbdff59448e8cf1d908ee0566
edneyRoldao <edneyroldao@gmail.com>
2016-10-05 17:42:17 -0300

    add cors in order to avoid sameOriginRequest

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # spring-restful-example
 
+http://viralpatel.net/blogs/embed-tomcat-maven-project-run-tomcat-maven/
+http://viralpatel.net/blogs/spring-4-mvc-rest-example-json/
 
--- spring-rest-example/src/main/java/com/edn/config/AppConfig.java
+++ spring-rest-example/src/main/java/com/edn/config/AppConfig.java
@@ -2,11 +2,23 @@ package com.edn.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.edn")
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/*").
+		allowedOrigins("*").
+		allowedHeaders("").
+		allowedMethods("DELETE", "POST", "PUT", "OPTIONS", "GET", "HEAD").
+		allowCredentials(true);
+		// http://dontpanic.42.nl/2015/04/cors-with-spring-mvc.html
+	}
 	
 }
